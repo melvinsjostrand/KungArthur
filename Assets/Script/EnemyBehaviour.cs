22819edbb11793b1ffcbcfb038388ff28a3125d4
BaoLanNguyen <nhlpool27@gmail.com>
2023-03-12 16:31:01 +0100

    ändringar

--- Assets/Script/EnemyBehaviour.cs
+++ Assets/Script/EnemyBehaviour.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    //patrullerande fiende
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+    //attackera
+    public float timeBetweenAttack;
+    bool alreadyAttacked;
+    //range
+    public float sightRange,attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+    private void awake(){
+      player = GameObject.Find("Player").transform;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //kontrollera sikt och attackräckvidd
+        playerInSightRange = Physics.CheckSphere(transform.position,sightRange,whatIsPlayer);
+        playerInAttackRange=  Physics.CheckSphere(transform.position,attackRange,whatIsPlayer);
     }
 }
