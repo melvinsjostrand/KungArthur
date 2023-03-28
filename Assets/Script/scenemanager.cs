4f253a3c3c02dc27d58bbcad8abf948c63bbf450
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-03-28 11:23:31 +0200

    startmeny

---
+++ Assets/Script/scenemanager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenemanager : MonoBehaviour
+{
+    [SerializeField] private int sceneIndex;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
