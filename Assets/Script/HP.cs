e5e4baf729493ae884d83f3df84720ba2372c229
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-04-10 18:59:41 +0200

    hp

---
+++ Assets/Script/HP.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+
+    public int maxHP = 100;
+    public int currentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
