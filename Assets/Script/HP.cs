c966dbaa494d5f8f4def0bb96584f24ae0f83a1c
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-04-12 12:18:46 +0200

    global volume

--- Assets/Script/HP.cs
+++ Assets/Script/HP.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class HP : MonoBehaviour
 {
-
+   // private Volume volume;
     public int maxHP = 100;
     public int currentHP;
 
     // Start is called before the first frame update
     void Start()
     {
+        //volume = GetComponent<Volume>();
         currentHP = maxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if(currentHP <= 40)
+       // {
+          //  vignette.intencity = 0.5;
+       // }
+        //else
+      //  {
+     //       vignette.intencity = 0;
+       // }
     }
 }
