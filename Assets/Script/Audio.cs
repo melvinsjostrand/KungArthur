f01297b84d51bf0d2d1dc7f18face30b3592ed5b
Pingvu <nhlpool27@gmail.com>
2023-04-25 14:35:45 +0200

    music

--- Assets/Script/Audio.cs
+++ Assets/Script/Audio.cs
@@ -1,20 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Audio : MonoBehaviour
 {
-    public AudioSource AudioSource;
-    private float Volume = 100f;
+    public GameObject ObjectMusic;
+    public Slider volumeSlider;
+    private AudioSource AudioSource;
+   
+
+    private float Volume = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        AudioSource.Play();
+        ObjectMusic = GameObject.FindWithTag("GameMusic");
+        AudioSource = ObjectMusic.GetComponent<AudioSource>();
+
+        Volume = PlayerPrefs.GetFloat("volume");
+
+        AudioSource.volume = Volume;
+        volumeSlider.value = Volume;
     }
 
     // Update is called once per frame
+    private void Update() 
+        {
+            AudioSource.volume = Volume;
+            PlayerPrefs.SetFloat("volume", Volume);
+        }
+    
 
     public void updateVolume(float volume){
         Volume = volume;
+    }
+
+    public void MusicReset()
+    {
+        PlayerPrefs.DeleteKey("volume");
+        AudioSource.volume = 1;
+        volumeSlider.value = 1;    
+    
     }
 }
